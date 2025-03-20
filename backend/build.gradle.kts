937636baf0ef9b5909e5730f9ece239321fb1a40
swjoon <jetkid1446@gmail.com>
2025-03-20 15:55:11 +0900

    chore : jacoco 설정 일부 수정

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -95,6 +95,55 @@ dependencies {
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
 }
 
+//tasks.jacocoTestCoverageVerification {
+//    violationRules {
+//        rule {
+//            // 'element'가 없으면 프로젝트의 전체 파일을 합친 값을 기준으로 한다.
+//            limit {
+//                // 'counter'를 지정하지 않으면 default는 'INSTRUCTION'
+//                // 'value'를 지정하지 않으면 default는 'COVEREDRATIO'
+//                minimum = "0.30".toBigDecimal()
+//            }
+//        }
+//
+//        rule {
+//            // 룰을 간단히 켜고 끌 수 있다.
+//            enabled = true
+//
+//            // 룰을 체크할 단위는 클래스 단위
+//            element = "CLASS"
+//
+//            // 브랜치 커버리지를 최소한 90% 만족시켜야 한다.
+//            limit {
+//                counter = "BRANCH"
+//                value = "COVEREDRATIO"
+//                minimum = "0.90".toBigDecimal()
+//            }
+//
+//            // 라인 커버리지를 최소한 80% 만족시켜야 한다.
+//            limit {
+//                counter = "LINE"
+//                value = "COVEREDRATIO"
+//                minimum = "0.80".toBigDecimal()
+//            }
+//
+//            // 빈 줄을 제외한 코드의 라인수를 최대 200라인으로 제한한다.
+//            limit {
+//                counter = "LINE"
+//                value = "TOTALCOUNT"
+//                maximum = "200".toBigDecimal()
+////              maximum = "8".toBigDecimal()
+//            }
+//
+//            // 커버리지 체크를 제외할 클래스들
+//            excludes = listOf(
+////                    "*.test.*",
+////                    "*.Kotlin*"
+//            )
+//        }
+//    }
+//}
+
 tasks.withType<Test> {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
